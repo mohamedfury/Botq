891632ba9b56f3dad512d5a3b4919f580892093d
mohamedfury <67813113+mohamedfury@users.noreply.github.com>
2025-07-09 03:24:31 +0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,14 @@
+-- config.lua
+
+return {
+  Token = "947886484:AAEh5j-gECZEN8BT45FDnPDgAQBEruA2FkE",
+  Sudo = {674291793}, -- ايدي المطور الاساسي
+  SudoUser = "@Mi_Mi", -- معرف المطور
+  DevName = "Fury Team", -- اسم الفريق
+  DevChannel = "t.me/hhvvv", -- قناة الدعم
+
+  -- خصائص عامة
+  Version = "1.0",
+  BotName = "FuryBot",
+  StartMsg = "↫ أهلاً بك في بوت الحماية والإدارة المطور من قبل Fury Team. استخدم الأوامر لبدء التحكم بالمجموعة.",
+}
